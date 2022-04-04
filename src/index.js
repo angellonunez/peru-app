fc0fd791dac5417627f2ccfb3b06fefe0e59ade3
Angello Nunez <angellonzgz@gmail.com>
2022-04-04 01:43:32 -0400

    Better version with React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+
+function App() {
+    return (
+        <div className="app">
+            <div className="header">
+                <Header />
+            </div>
+            <div className="main">
+                <Main />
+            </div>
+            <div className="footer">
+                <Footer />
+            </div>
+        </div>
+    );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
